c9ad5883d1406de07d2d4d10e1445675f2370185
Dan Stronge <dnlstronge@gmail.com>
2023-05-13 11:55:16 +0100

    [x] - add logic

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,16 +1,37 @@
 import classes from './Counter.module.css';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch} from 'react-redux';
+
 
 const Counter = () => {
-
+  const dispatch = useDispatch()
   const counter = useSelector((state: any) => state.counter);
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
+    
+    const incrementHandler = () => {
+      dispatch({type: "increment"})
+    }
+    const decrementHandler = () => {
+      dispatch({type: "decrement"})
+    }
+    
+    if(e.currentTarget.value === "increment") {
+      incrementHandler()
+    }
+    if(e.currentTarget.value === "decrement") {
+      decrementHandler()
+    }
+   
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={toggleCounterHandler} value="increment">+</button>
+        <button onClick={toggleCounterHandler} value="decrement">-</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
