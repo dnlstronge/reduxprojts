436c3b7e177856d7f29ca9a7d55d112abb42f407
Dan Stronge <dnlstronge@gmail.com>
2023-05-14 10:43:16 +0100

    [x] - add new case to reducer

--- src/Store/index.ts
+++ src/Store/index.ts
@@ -1,20 +1,31 @@
 import { createStore } from "redux";
 
-const counterReducer = (state = {counter: 0}, action: {type: string, payload: number}) => {
+const counterReducer = (state = {counter: 0, showCounter: false}, action: {type: string, payload: number}) => {
+
+    if(action.type === "showCounter") {
+        return {
+            counter: state.counter,
+            showCounter: true
+        }
+    }
     if(action.type === "increment") {
         return {
-            counter: state.counter + 1
+            counter: state.counter + 1,
+            showCounter: true
+
         }
     }
     if(action.type === "decrement") {
         return {
-            counter: state.counter -1
+            counter: state.counter -1,
+            showCounter: true
         }
     }
     if(action.type === "increase") {
         
         return {
-            counter: state.counter + action.payload
+            counter: state.counter + action.payload,
+            showCounter: true
         }
     }
     return state;
