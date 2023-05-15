de35eee3a4111c9c5558d12ce1f98c0df377f9c5
Dan Stronge <dnlstronge@gmail.com>
2023-05-15 08:39:03 +0100

    [x] - remove reducer

--- src/Store/index.ts
+++ src/Store/index.ts
@@ -29,36 +29,7 @@ const counterSlice = createSlice({
         }
 })
 
-const counterReducer = (state = intitialState, action: {type: string, payload: number}) => {
 
-    if(action.type === "showCounter") {
-        return {
-            counter: state.counter,
-            showCounter: !state.showCounter
-        }
-    }
-    if(action.type === "increment") {
-        return {
-            counter: state.counter + 1,
-            showCounter: true   
-
-        }
-    }
-    if(action.type === "decrement") {
-        return {
-            counter: state.counter -1,
-            showCounter: true
-        }
-    }
-    if(action.type === "increase") {
-        
-        return {
-            counter: state.counter + action.payload,
-            showCounter: true
-        }
-    }
-    return state;
-}
 
 const store = createStore(counterReducer)
 
