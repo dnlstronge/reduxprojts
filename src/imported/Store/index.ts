1927a499ae1d3d6193a4f508b6af8edbd6a00ebc
Dan Stronge <dnlstronge@gmail.com>
2023-05-15 08:16:46 +0100

    [x] - install redux tooklkit [x] - remove redux from json

---
+++ src/imported/Store/index.ts
@@ -0,0 +1,43 @@
+import { createStore } from "redux";
+
+export type initS = {
+    counter: number,
+    showCounter: boolean
+}
+
+const intitialState: initS = {counter: 0, showCounter: false }
+
+const counterReducer = (state = intitialState, action: {type: string, payload: number}) => {
+
+    if(action.type === "showCounter") {
+        return {
+            counter: state.counter,
+            showCounter: !state.showCounter
+        }
+    }
+    if(action.type === "increment") {
+        return {
+            counter: state.counter + 1,
+            showCounter: true   
+
+        }
+    }
+    if(action.type === "decrement") {
+        return {
+            counter: state.counter -1,
+            showCounter: true
+        }
+    }
+    if(action.type === "increase") {
+        
+        return {
+            counter: state.counter + action.payload,
+            showCounter: true
+        }
+    }
+    return state;
+}
+
+const store = createStore(counterReducer)
+
+export default store;
