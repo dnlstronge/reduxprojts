ce41c418d3eb4cdd3ceedb79927f14934f927b57
Dan Stronge <dnlstronge@gmail.com>
2023-05-17 10:24:37 +0100

    [x] - remove comment & redundant

--- src/Store/index.ts
+++ src/Store/index.ts
@@ -10,28 +10,10 @@ export type initA = {
     isAuthenticated: boolean
 }
 
-/*Initial states */
-//const intitialCounterState: initS = {counter: 0, showCounter: false }
+
 const initialAuthState: initA = {isAuthenticated: false}
 
-// const counterSlice = createSlice({
-//     name: "counter",
-//     initialState: intitialCounterState,
-//     reducers: {
-//         increment(state){
-//             state.counter ++
-//         },
-//         decrement(state){
-//             state.counter --
-//         },
-//         increase(state, action){
-//             state.counter = state.counter + action.payload
-//         },
-//         toggleCounter(state){
-//             state.showCounter = !state.showCounter
-//         }
-//         }
-// })
+
 
 const authSlice = createSlice({
     name: "authenication",
