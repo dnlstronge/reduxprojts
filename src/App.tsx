e8ab9b795d40c8190b653658266bbbf0d0b9460b
Dan Stronge <dnlstronge@gmail.com>
2023-05-17 09:56:24 +0100

    [x] - user profile renders conditionally based on redux state slice

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Counter from './components/Counter';
 import Header from './components/Header';
 import Auth from './components/Auth';
+import UserProfile from './components/UserProfile';
 import { useSelector } from "react-redux"
 import { initA, initS } from './Store';
 
@@ -17,6 +18,8 @@ function App() {
      <Header />
      {!isAuth && 
      <Auth />}
+     {isAuth && 
+     <UserProfile />}
      <Counter />
     </>
    
